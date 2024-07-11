710ee58c9089a204bf5b64ae22d801097e8a0ca5
Oliver Shaw <OllieS@portland-fuel.co.uk>
2024-07-11 13:45:43 +0100

    Latest

--- Fuelcards/wwwroot/js/CustomerDetails.js
+++ Fuelcards/wwwroot/js/CustomerDetails.js
@@ -35,10 +35,18 @@ function ShowNetworkAccountNumberInput(element){
     const ParentOfParent = ParentElement.parentElement;
 
 
-    const id = element.value + "AccountNumInput";
+    const AccountNumInputID = element.value + "AccountNumInput";
+    const AddonNumInputID = element.value + "AddonInput";
+    const DateInputID = element.value + "DateInput";
     
     if(!element.checked){
-        document.querySelectorAll("#" + id).forEach(element => {
+        document.querySelectorAll("#" + AccountNumInputID).forEach(element => {
+            element.remove();
+        });
+        document.querySelectorAll("#" + AddonNumInputID).forEach(element => {
+            element.remove();
+        });
+        document.querySelectorAll("#" + DateInputID).forEach(element => {
             element.remove();
         });
     }
@@ -47,7 +55,147 @@ function ShowNetworkAccountNumberInput(element){
         InputElement.type = "text";
         InputElement.id = element.value + "AccountNumInput";
         InputElement.required = true;
-        InputElement.placeholder = "Enter Account Number";
+        var placeholderTxt = element.value + " Account Number";
+        InputElement.placeholder = placeholderTxt;
         ParentOfParent.appendChild(InputElement);    
+
+
+        const AddonElement = document.createElement("input");
+        AddonElement.type = "text";
+        AddonElement.id = element.value + "AddonInput";
+        AddonElement.required = true;
+        var addonPlaceholderTxt = element.value + " Addon";
+        AddonElement.placeholder = addonPlaceholderTxt;
+        ParentOfParent.appendChild(AddonElement);   
+
+        const DateElement = document.createElement("input");
+        DateElement.type = "date";
+        DateElement.id = element.value + "DateInput";
+        DateElement.required = true;
+        var datePlaceholderTxt = element.value + " Date";
+        DateElement.placeholder = datePlaceholderTxt;
+        ParentOfParent.appendChild(DateElement);  
     }
 }
+
+
+function CloseNewFixOverlay(){
+    RemoveSelectElementFromNewFixForm();
+
+
+    var overlayElement = document.getElementById("overlay");
+
+    overlayElement.hidden = true;
+}
+
+function ShowNewFixForm(){
+    const SelectEleForUsers = GetSelectOptionForUserForNewFixForm();
+
+    if (SelectEleForUsers.options.length === 0) {
+        Swal.fire({
+            icon: 'warning',
+            title: 'No network options available',
+            text: 'Please select at least one network network.',
+        });
+    } else {
+        const parentElement = document.getElementById("NewFixSelectParent");
+        parentElement.appendChild(SelectEleForUsers);
+        var overlayElement = document.getElementById("overlay");
+        overlayElement.hidden = false;
+    }
+   
+}
+
+function GetSelectOptionForUserForNewFixForm(){
+    const NetworkCheckBoxes = document.querySelectorAll(".NetworkCheck");
+    var Options = [];
+    NetworkCheckBoxes.forEach(element => {
+        if(element.checked){
+            Options.push(element.value);
+        }
+    });
+
+    const selectElement = document.createElement("select");
+    selectElement.id = "NewFixNetworkSelect";
+    Options.forEach(option => {
+        const optionElement = document.createElement("option");
+        optionElement.value = option;
+        optionElement.text = option;
+        selectElement.appendChild(optionElement);
+    });
+
+    return selectElement;
+}
+
+function RemoveSelectElementFromNewFixForm(){
+    const selectElement = document.getElementById("NewFixNetworkSelect");
+    selectElement.remove();
+}
+
+function AddNewFix(event){
+    event.preventDefault();
+
+    const form = document.getElementById("NewFixForm");
+    if (form) {
+        const formData = new FormData(form);
+        const values = Object.fromEntries(formData.entries());
+        
+        const selectElement = document.getElementById("NewFixNetworkSelect");
+        const selectedOption = selectElement.options[selectElement.selectedIndex].value;
+        values["network"] = selectedOption;
+        
+        const periodElement = document.getElementById("period");
+        const selectedPeriod = periodElement.options[periodElement.selectedIndex].value;
+        values["period"] = selectedPeriod;
+        
+        const gradeElement = document.getElementById("grade");
+        const selectedGrade = gradeElement.options[gradeElement.selectedIndex].value;
+        values["grade"] = selectedGrade;
+        console.log(values);
+        
+        var NetworkUserSelected = values["network"];
+        var FixsListToAddTo = GetFixListToAddTo(NetworkUserSelected);
+        console.log("Adding the fix to the network of " + NetworkUserSelected);
+        FixsListToAddTo.push(values); 
+        ChangeLabelToShowPlus1Fix(NetworkUserSelected);
+        console.log("KeyFuelsFixs: " + KeyFuelsFixs);
+        
+        
+    }
+
+
+
+       
+}
+
+function GetFixListToAddTo(NetworkUserSelected){
+    switch(NetworkUserSelected){
+        case "KeyFuels":
+            return KeyFuelsFixs;
+        case "Texaco":
+            return TexacoFixs;
+        case "UkFuels":
+            return UkFuelsFixs;
+        case "FuelGenie":
+            return FuelGenieFixs;
+    }
+}
+function ChangeLabelToShowPlus1Fix(NetworkUserSelected){
+    const labelElement = document.getElementById(NetworkUserSelected + "Span");
+    var SpanText = labelElement.innerText
+    if(SpanText === "No Fixes"){
+        labelElement.textContent = "1 Fix";
+    }
+    else{
+        var CurrentFixNum = SpanText.split(" ")[0];
+        var NewFixNum = parseInt(CurrentFixNum) + 1;
+        labelElement.textContent = NewFixNum + " Fixes";
+    }
+}
+
+
+const KeyFuelsFixs = []
+const TexacoFixs = []
+const UkFuelsFixs = []
+const FuelGenieFixs = []
+
