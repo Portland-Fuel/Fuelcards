858a3ca20cd7df39c57e01011ae400f30c8c31bc
Connor Wilson <Connor1199@hotmail.co.uk>
2024-07-11 09:59:07 +0100

    .

--- Fuelcards/Controllers/HomeController.cs
+++ Fuelcards/Controllers/HomeController.cs
@@ -13,6 +13,7 @@ namespace Fuelcards.Controllers
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
+
         {
             _logger = logger;
         }
